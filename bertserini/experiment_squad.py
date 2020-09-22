f6b11fb540d774d5c46cd5a8b91bf31717b4b975
xueguang <x93ma@edu.uwaterloo.ca>
2020-09-22 13:44:02 +0800

    add experiment_squad.py

---
+++ bertserini/experiment_squad.py
@@ -0,0 +1,27 @@
+import json
+from tqdm import tqdm
+from .readers import BERT
+from .pyserini_retriever import retriever, build_searcher
+from .utils_new import extract_squad_questions
+
+if __name__ == "__main__":
+
+    questions = extract_squad_questions("squad_dev.json")
+    bert_reader = BERT("rsvp-ai/bertserini-bert-base-squad", "rsvp-ai/bertserini-bert-base-squad")
+    searcher = build_searcher("index/lucene-index.enwiki-20180701-paragraphs")
+
+    all_answer = []
+    for question in tqdm(questions):
+        contexts = retriever(question, searcher, 10)
+        final_answers = bert_reader.predict(question, contexts)
+        final_answers_lst = []
+        for ans in final_answers:
+            final_answers_lst.append({
+                {"id": question.id,
+                 "answer": ans.text,
+                 "phrase_score": ans.score,
+                 "paragraph_score": ans.ctx_score,
+                 }
+            })
+        all_answer.append(final_answers_lst)
+    json.dump(all_answer, open("result_bert_base.json", 'w'))
--- bertserini/utils_new.py
+++ bertserini/utils_new.py
@@ -1,4 +1,22 @@
+import json
+from .base import Question, Answer
+
+
 def get_best_answer(candidates, weight=0.5):
     for ans in candidates:
         ans.aggregate_score(weight)
     return candidates.sorted(key=lambda x: x.total_score, reverse=True)[0]
+
+
+def extract_squad_questions(squad_filename):
+    data = json.load(open(squad_filename, 'r'))
+    data = data["data"]
+    questions = []
+    for article in data:
+        for paragraph in article["paragraphs"]:
+            for qa in paragraph["qas"]:
+                id_ = qa["id"]
+                question = qa["question"]
+                questions.append(Question(question, id_))
+    return questions
+
