48db4643d2d55a60d4c208578b0a96f9823beaee
xueguang <x93ma@edu.uwaterloo.ca>
2020-09-25 06:29:22 +0800

    make bert args configurable

--- bertserini/reader/bert_reader.py
+++ bertserini/reader/bert_reader.py
@@ -40,6 +40,26 @@ class BERT(Reader):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.model = AutoModelForQuestionAnswering.from_pretrained(model_name).to(self.device).eval()
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, do_lower_case=True)
+        self.args = {
+            "max_seq_length": 384,
+            "doc_stride": 128,
+            "max_query_length": 64,
+            "threads": 1,
+            "tqdm_enabled": False,
+            "n_best_size": 20,
+            "max_answer_length": 30,
+            "do_lower_case": True,
+            "output_prediction_file": False,
+            "output_nbest_file": None,
+            "output_null_log_odds_file": None,
+            "verbose_logging": False,
+            "version_2_with_negative": True,
+            "null_score_diff_threshold": 0,
+        }
+
+    def update_args(self, args_to_change):
+        for key in args_to_change:
+            self.args[key] = args_to_change[key]
 
     def predict(self, question: Question, contexts: List[Context]) -> List[Answer]:
         examples = craft_squad_examples(question, contexts)
@@ -47,13 +67,13 @@ class BERT(Reader):
         features, dataset = squad_convert_examples_to_features(
             examples=examples,
             tokenizer=self.tokenizer,
-            max_seq_length=384,
-            doc_stride=128,
-            max_query_length=64,
+            max_seq_length=self.args["max_seq_length"],
+            doc_stride=self.args["doc_stride"],
+            max_query_length=self.args["max_query_length"],
             is_training=False,
             return_dataset="pt",
-            threads=1,
-            tqdm_enabled=False
+            threads=self.args["threads"],
+            tqdm_enabled=self.args["tqdm_enabled"]
         )
 
         # Note that DistributedSampler samples randomly
@@ -89,15 +109,15 @@ class BERT(Reader):
             all_examples=examples,
             all_features=features,
             all_results=all_results,
-            n_best_size=20,
-            max_answer_length=30,
-            do_lower_case=True,
-            output_prediction_file=None,
-            output_nbest_file=None,
-            output_null_log_odds_file=None,
-            verbose_logging=False,
-            version_2_with_negative=False,
-            null_score_diff_threshold=0,
+            n_best_size=self.args["n_best_size"],
+            max_answer_length=self.args["max_answer_length"],
+            do_lower_case=self.args["do_lower_case"],
+            output_prediction_file=self.args["output_prediction_file"],
+            output_nbest_file=self.args["output_nbest_file"],
+            output_null_log_odds_file=self.args["output_null_log_odds_file"],
+            verbose_logging=self.args["verbose_logging"],
+            version_2_with_negative=self.args["version_2_with_negative"],
+            null_score_diff_threshold=self.args["null_score_diff_threshold"],
             tokenizer=self.tokenizer,
             language=question.language
         )
