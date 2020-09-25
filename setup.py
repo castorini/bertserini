17d0fb27bc199a6d37c2a5528ea9cb15ece2ef42
xueguang <x93ma@edu.uwaterloo.ca>
2020-09-25 22:40:20 +0800

    fix requirements.txt

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='bertserini',
-    version='0.0.1',
+    version='0.0.2',
     packages=['bertserini', 'bertserini.reader', 'bertserini.retriever', 'bertserini.experiments', 'bertserini.utils'],
     url='https://github.com/rsvp-ai/bertserini',
     license='',
